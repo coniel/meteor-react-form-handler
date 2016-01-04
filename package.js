3e22e790bfed76f592282db34a16253991fb1ef3
Oscar Coniel <oscar@coniel.com>
2016-01-05 01:36:58 +0200

    Version bump

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: "coniel:react-form-handler",
   summary: "React form validation using aldeed:simple-schema" ,
   git: 'https://github.com/coniel/meteor-react-form-handler.git',
-  version: "0.3.0"
+  version: "0.3.1"
 });
 
 Package.onUse(function(api) {
